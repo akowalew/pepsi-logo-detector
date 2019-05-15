2b51bf26c9d74cf80d43c3493063d153469db8e8
akowalew <ram.techen@gmail.com>
2019-05-15 16:14:04 +0200

    Moved common types declarations to separate file

--- lib/include/points.hpp
+++ lib/include/points.hpp
@@ -4,15 +4,7 @@
 
 #include <opencv2/opencv.hpp>
 
-using Point = cv::Point;
-
-using Points = std::vector<cv::Point>;
-
-using PointsSet = std::vector<Points>;
-
-using Rect = cv::Rect;
-
-using Rects = std::vector<cv::Rect>;
+#include "types.hpp"
 
 Rect calc_bounding_rect(const Points& points);
 
---
+++ lib/include/types.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+
+using Point = cv::Point;
+
+using Points = std::vector<cv::Point>;
+
+using PointsSet = std::vector<Points>;
+
+using Rect = cv::Rect;
+
+using Rects = std::vector<cv::Rect>;
